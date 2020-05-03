3cdb12a68a0ecfba52eeba53714642f2d84ca3d3
parang <appdler96@kaist.ac.kr>
2020-05-03 22:13:01 +0900

    feat(toDo): update to-do-list with redux using vanilla-js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,26 @@ const ul = document.querySelector('ul');
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
-const reducer = (state, action) => {
-  console.log(action)
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
+
+const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: Date.now() }];
     case DELETE_TODO:
-      return [];
+      return state.filter(toDo => toDo.id !== action.id);
     default:
       return state;
   }
@@ -21,12 +34,38 @@ const reducer = (state, action) => {
 
 const store = createStore(reducer);
 
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text));
+}
+
+const dispatchDeleteToDo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+}
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = "DEL";
+    btn.addEventListener('click', dispatchDeleteToDo);
+    li.innerText = toDo.text;
+    li.id = toDo.id;
+    ul.appendChild(li);
+    li.appendChild(btn);
+  })
+}
+
+store.subscribe(paintToDos);
+store.subscribe(() => console.log(store.getState()));
+
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  console.log(toDo);
-  store.dispatch({ type: ADD_TODO, text: toDo })
+  dispatchAddToDo(toDo);
 }
 
 form.addEventListener('submit', onSubmit);
